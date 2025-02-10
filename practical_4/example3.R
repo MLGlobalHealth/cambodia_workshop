e8654baee865c3413126d640def9684c69b8cb65
Ettie Unwin <h.unwin@imperial.ac.uk>
2025-02-10 11:31:07 +0700

    Adds example 3

---
+++ practical_4/example3.R
@@ -0,0 +1,47 @@
+## This file compiles and plots an odin model 
+library(odin)
+library(ggplot2)
+library(tidyr)
+
+run_example_3_model = function(N, I_init, beta, sigma, u, max_t){
+  # The file name of the model
+  model <- "practical_4/odin_models/example3.R"
+  
+  # Compiles the model
+  generator <- odin::odin(model)
+  mod <- generator$new(user = list(sigma = sigma, beta = beta, u = u, 
+                                   N = N, I_init = I_init))
+  
+  
+  # Set time variable
+  tt <- seq(0, max_t, by = 0.5)
+  
+  # Runs the model
+  y <- mod$run(tt)
+  return (y)
+}
+
+data = run_example_3_model(N = 1000,
+                           I_init = 1,
+                           beta = 2,
+                           sigma = 0.3,
+                           u = 10,
+                           max_t = 100)
+
+data_long = gather(data.frame(data), 
+                   key = variable, 
+                   value = value, 
+                   -t)
+
+p = ggplot(data_long) +
+  geom_line(aes(t, value, col = variable), linewidth = 2) + 
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0)) + 
+  xlab("Time") + ylab("Number of people") +
+  theme_bw() +
+  theme(legend.title=element_blank(), 
+        axis.text.x = element_text(size=16),
+        axis.text.y = element_text(size=16),
+        text = element_text(size = 16))
+
+print(p)
